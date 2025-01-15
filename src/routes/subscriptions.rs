dd2dd36402ab792cd1e985c7c1d0e2e5e6103620
Michael Al Tork <michael.altork@gmail.com>
2025-01-15 10:39:37 +0100

    add connection as application state

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use sqlx::PgConnection;
 
 #[derive(Deserialize)]
 pub struct FormData {
@@ -7,6 +8,9 @@ pub struct FormData {
     pub name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    _form: web::Form<FormData>,
+    _connection: web::Data<PgConnection>,
+) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
