a22b6c883231fe29a2e5a38be8bf99784263fcd4
Michael Al Tork <michael.altork@gmail.com>
2025-01-16 09:37:17 +0100

    added logs to the program

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
-use std::net::TcpListener;
-
+use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
+use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
@@ -9,6 +9,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
